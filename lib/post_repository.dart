e4758eb5a27adce450cd7d82f6b6aed8a7f1837f
Jeonkukil <a0211a@naver.com>
2023-04-03 15:06:47 +0900

    end

--- lib/post_repository.dart
+++ lib/post_repository.dart
@@ -1,17 +1,18 @@
 import 'package:dio/dio.dart';
+import 'package:http_app/user.dart';
 
 class PostRepository {
   final dio = Dio();
 
-  Future<void> findById(int id) async {
+  Future<Post> findById(int id) async {
     Response response = await dio.get("https://jsonplaceholder.typicode.com/posts/$id");
-    Map<String, dynamic> responseMap = response.data;
-    print(responseMap["userId"]);
+    Post post = Post.fromJson(response.data);
+    return post;
   }
 
-
-  Future<void> findAll() async {
+  Future<List<Post>> findAll() async {
     Response response = await dio.get("https://jsonplaceholder.typicode.com/posts");
-    print(response.data);
+    List<dynamic> mapList = response.data;
+    return mapList.map((e) => Post.fromJson(e)).toList();
   }
 }
